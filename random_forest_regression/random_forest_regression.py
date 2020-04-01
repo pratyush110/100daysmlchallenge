9c7624797cd0615d263c0a5fe8aafff4cde42d7c
pratyush110 <pratyush.priyadarshi11@gmail.com>
2020-04-01 23:49:52 +0530

    update

--- random_forest_regression/random_forest_regression.py
+++ random_forest_regression/random_forest_regression.py
@@ -10,10 +10,6 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# Splitting the dataset into the Training set and Test set
-"""from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
-
 # Fitting Random Forest Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
